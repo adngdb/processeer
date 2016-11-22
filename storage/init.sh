969884cb64d812ff46c641f67246555745dc1b65
Adrian Gaudebert <adrian@gaudebert.fr>
2016-11-22 18:11:08 +1100

    Use correct bucket in storage init.sh.

--- storage/init.sh
+++ storage/init.sh
@@ -7,8 +7,8 @@ curl -XPUT 'localhost:8888/v1/buckets/processeer' --user $AUTH
 
 # Create a collection for blocks with restricted permissions.
 echo '{"permissions": {"record:create": ["system.Authenticated"], "read": ["system.Everyone"]}}' | \
-    curl -XPUT 'localhost:8888/v1/buckets/spectateur/collections/blocks' --user $AUTH
+    curl -XPUT 'localhost:8888/v1/buckets/processeer/collections/blocks' --user $AUTH
 
 # Create a collection for reports with restricted permissions.
 echo '{"permissions": {"record:create": ["system.Authenticated"], "read": ["system.Everyone"]}}' | \
-    curl -XPUT 'localhost:8888/v1/buckets/spectateur/collections/reports' --user $AUTH
+    curl -XPUT 'localhost:8888/v1/buckets/processeer/collections/reports' --user $AUTH
