c2be3b2a717512d4a9230493fb91a6d0bb3dae86
Adrian Gaudebert <adrian@gaudebert.fr>
2017-04-27 16:30:05 +0200

    More beautiful Sign in component.

--- client/src/components/user-signin.jsx
+++ client/src/components/user-signin.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Glyphicon } from 'react-bootstrap';
+import { Link } from 'react-router';
 
 
 export default function UserSignin({ user, signin, signout }) {
     if (user && !user.isFetching && !user.didInvalidate && user.authToken) {
         // A user is signed in.
-        const signOut = <span> <Button onClick={signout}>Sign out</Button></span>;
+        const signOut = (
+            <Link to="/" onClick={signout}>
+                <Glyphicon glyph="log-out" /> Sign out
+            </Link>
+        );
         if (user.username) {
-            return (<span>{`Signed in as ${user.username}`}{signOut}</span>);
+            return (<span>Signed in as <strong>{user.username}</strong> - {signOut}</span>);
         }
 
-        return <span>Signed in{signOut}</span>;
+        return <span>Signed in - {signOut}</span>;
     }
 
     if (user && user.isFetching) {
-        return (<Button>Signing in</Button>);
+        return (<Button><Glyphicon glyph="time" /> Signing in</Button>);
     }
 
-    return (<Button onClick={signin}>Sign in</Button>);
+    return (
+        <Button onClick={signin}>
+            <Glyphicon glyph="log-in" /> Sign in with GitHub
+        </Button>
+    );
 }
