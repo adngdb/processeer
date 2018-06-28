a600e554ebe3899eb8e35619c5c2e121578fc7ab
Adrian Gaudebert <adrian@gaudebert.fr>
2018-06-28 11:54:45 +0200

    Refactor docker-compose to use in prod and dev.

--- storage/init.sh
+++ storage/init.sh
@@ -2,16 +2,18 @@
 
 AUTH="$KINTO_ADMIN_USER:$KINTO_ADMIN_PASSWORD"
 
-# Create the processeer bucket.
+echo "Create the processeer bucket."
 curl -XPUT 'localhost:8888/v1/buckets/processeer' --user $AUTH
 
-# Create a collection for blocks with restricted permissions.
+echo ''
+echo "Create a collection for blocks with restricted permissions."
 curl -XPUT 'localhost:8888/v1/buckets/processeer/collections/blocks' \
     --user $AUTH \
     -H "Content-Type: application/json" \
     -d '{"permissions": {"write": ["system.Authenticated"], "read": ["system.Everyone"]}}'
 
-# Create a collection for reports with restricted permissions.
+echo ''
+echo "Create a collection for reports with restricted permissions."
 curl -XPUT 'localhost:8888/v1/buckets/processeer/collections/reports' \
     --user $AUTH \
     -H "Content-Type: application/json" \
